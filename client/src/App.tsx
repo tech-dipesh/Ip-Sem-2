371156814f89a490487acece212750ee96ae649c
Dipesh <dipeshsharmagautam@outlook.com>
2025-05-06 12:42:29 +0530

    add the comments on the app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,6 +20,7 @@ const App: React.FC = () => {
             path="/upload"
             element={
               <>
+              {/* Authentication for the clerk */}
                 <SignedIn>
                   <Upload />
                 </SignedIn>
